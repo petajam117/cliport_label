522da5bad2c96c644c5cc2de80a4b33bbcae55d1
petajam117 <mikaelpetaja@gmail.com>
2023-06-30 13:57:46 +0300

    added multi task support, recover functionality, changed home pose to panda ready & other fixes

--- src/cliport_label/console.py
+++ src/cliport_label/console.py
@@ -1,26 +1,52 @@
 """CLI entrypoints for cliport_label"""
-import logging
+from pathlib import Path
 
 import click
+import toml
 
-from libadvian.logging import init_logging
 from cliport_label import __version__
+from cliport_label.main import main_tool, main_viewer, main_editor
 
 
-LOGGER = logging.getLogger(__name__)
 
-
-@click.command()
+@click.group()
 @click.version_option(version=__version__)
 @click.option("-l", "--loglevel", help="Python log level, 10=DEBUG, 20=INFO, 30=WARNING, 40=CRITICAL", default=30)
 @click.option("-v", "--verbose", count=True, help="Shorthand for info/debug loglevel (-v/-vv)")
-def cliport_label_cli(loglevel: int, verbose: int) -> None:
+@click.argument("config-path", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.pass_context
+def cliport_label_cli(ctx, config_path: Path, loglevel: int, verbose: int,) -> None:
     """GUI application for recording demonstration for CLIPort project """
-    if verbose == 1:
-        loglevel = 20
-    if verbose >= 2:
-        loglevel = 10
-    init_logging(loglevel)
-    LOGGER.setLevel(loglevel)
+    ctx.ensure_object(dict)
+    config = toml.load(config_path)
+    ctx.obj['CONFIG'] = config
 
-    click.echo("Do your thing")
+
+@cliport_label_cli.command()
+@click.pass_context
+def tool(ctx):
+    """Run label tool"""
+    config = ctx.obj['CONFIG']
+    click.echo("GUI Controls")
+    for key, value in config['tool_controlkeys'].items():
+        click.echo("Press " + click.style(value, fg='green', bold=True) + " to " + click.style(key, fg='white', bold=True))
+    main_tool(config)
+
+@cliport_label_cli.command()
+@click.pass_context
+def viewer(ctx):
+    """Run data viewer"""
+    config = ctx.obj['CONFIG']
+
+    main_viewer(config)
+
+
+@cliport_label_cli.command()
+@click.pass_context
+@click.argument("filepath", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.option("-l", "--lang-goal", type=str, default="", help="Specify new language goal to set")
+def editor(ctx, filepath: Path, lang_goal: str):
+    """Run data editor"""
+    click.echo(f"Editing file: {filepath}")
+    main_editor(filepath, lang_goal)
+
---
+++ src/cliport_label/gui.py
@@ -0,0 +1,340 @@
+"""GUI for label tool"""
+from enum import Enum
+from pathlib import Path
+import pickle
+
+import cv2
+import numpy as np
+import rospy
+
+from cliport_label.taskexecutor import TaskInfo
+from cliport_label.utils import get_line_theta, depth_to_heatmap, draw_on_disp_img
+
+class StreamType(Enum):
+    RGB = 0
+    DEPTH = 1
+
+class ToolType(Enum):
+    PICKBBOX = 0
+    PICKROTATE = 1
+    PLACEBBOX = 2
+    PLACEROTATE = 3
+    NONE = 4
+
+class ToolGUI:
+    """Visualize and interact with camera stream and snapshot window"""
+
+    def __init__(self, config, streamer, taskexecutor) -> None:
+        """Initialize stuff"""
+        self.stream_win = "stream"
+        self.snapshot_win = "snapped_observation"
+        self.stream_type = StreamType.RGB
+        self.snapshot = None
+        self.lang_goal = ""
+        self.pick_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+        self.place_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+        self.pick_data_list = []
+        self.place_data_list = []
+        self.selected_tool = ToolType.PICKBBOX
+        self.task = taskexecutor
+        self.streamer = streamer
+        self.controls = config["tool_controlkeys"]
+        self.output = config['output']
+        # Number of discrete rotation angles
+        self.rotation_angles = 10
+        if self.task is not None:
+            self.task.rotation_angles = self.rotation_angles
+        cv2.namedWindow(self.snapshot_win, cv2.WINDOW_AUTOSIZE | cv2.WINDOW_GUI_NORMAL)
+        cv2.namedWindow(self.stream_win)
+        # highgui function called when mouse events occur
+        cv2.setMouseCallback(self.snapshot_win, self.get_coords)
+
+    def cleanup(self):
+        """Release resources"""
+        cv2.destroyAllWindows()
+        if self.task:
+            self.task.cleanup()
+
+    def run(self) -> None:
+        """Run GUI"""
+        self.handle_stream()
+        if self.snapshot:
+            self.handle_snapshot()
+        key_press = cv2.waitKey(1) & 0xFF
+        self.handle_keypress(key_press)
+
+    def handle_stream(self) -> None:
+        """Display and interact with stream window"""
+        if self.streamer.rgb is not None and self.stream_type is StreamType.RGB:
+            # OpenCV handles bgr format instead of rgb so we convert first
+            bgr = cv2.cvtColor(self.streamer.rgb, cv2.COLOR_RGB2BGR)
+            cv2.imshow(self.stream_win, bgr)
+        if self.streamer.depth is not None and self.stream_type is StreamType.DEPTH:
+            cv2.imshow(self.stream_win, depth_to_heatmap(self.streamer.depth))
+
+    def handle_snapshot(self) -> None:
+        """Display and interact with snapshot"""  
+        snap_type = self.stream_type is not StreamType.RGB
+        disp_img = self.snapshot[snap_type]
+        # convert mono depth to color heatmap before displaying
+        if self.snapshot[snap_type].ndim == 2:
+            disp_img = depth_to_heatmap(disp_img)
+        else:
+            disp_img = cv2.cvtColor(disp_img, cv2.COLOR_RGB2BGR)
+        # Draw and display image
+        rot_color = (0, 0, np.iinfo(disp_img.dtype).max)
+        pick_bbox_color = (0, np.iinfo(disp_img.dtype).max, 0)
+        place_bbox_color = (np.iinfo(disp_img.dtype).max, 0, 0)
+        draw_on_disp_img(disp_img,self.pick_data, pick_bbox_color, rot_color)
+        draw_on_disp_img(disp_img,self.place_data, place_bbox_color, rot_color)
+        cv2.imshow(self.snapshot_win, disp_img)
+
+    def handle_keypress(self, key_press) -> None:
+        """Handle keypress in gui window"""
+        # Stream related controls
+        if key_press == ord(self.controls['rgb']):
+            self.stream_type = StreamType.RGB
+        if key_press == ord(self.controls['depth']):
+            self.stream_type = StreamType.DEPTH
+        if key_press == ord(self.controls['snapshot']):
+            self.snapshot = (self.streamer.rgb.copy(), self.streamer.depth.copy())
+            rospy.loginfo("Capturing snapshot from camera stream")
+        # Snapshot related controls
+        if key_press == ord(self.controls['clear_pick']):
+            self.task.reset_pick()
+            self.pick_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+            rospy.loginfo("Clearing up pick data")
+        if key_press == ord(self.controls['clear_place']):
+            self.task.reset_place()
+            self.place_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+            rospy.loginfo("Clearing up place data")
+        if key_press == ord(self.controls['clear_all']):
+            self.task.reset_task()
+            self.pick_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+            self.place_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+            self.pick_data_list = []
+            self.place_data_list = []
+            self.lang_goal = ""
+            rospy.loginfo("All task data cleared")
+        if key_press == ord(self.controls['save']):
+            self.save_demo()
+        if key_press == ord(self.controls['lang_goal']):
+            self.lang_goal = input("Enter language goal: ")
+            rospy.loginfo(f"Setting {self.lang_goal = } for demonstration...")
+        if key_press == ord(self.controls['quit']):
+            raise KeyboardInterrupt
+        if key_press == ord(self.controls['recover_robot']):
+            self.task.recover()
+        # Ignore task executor related commands if it's set to None
+        if self.task is not None:
+            if key_press == ord(self.controls['pick']):
+                data = self.pick_data
+                if len(data['bbox']) == 2 and len(data['rotline']) == 2:
+                    tinfo = TaskInfo(self.snapshot[0], self.snapshot[1], data['bbox'], data['rotation'])
+                    rospy.loginfo("Executing pick task")
+                    self.task.pick(tinfo)
+            if key_press == ord(self.controls['place']):
+                data = self.place_data
+                if len(data['bbox']) == 2 and len(data['rotline']) == 2:
+                    tinfo = TaskInfo(self.snapshot[0], self.snapshot[1], data['bbox'], data['rotation'])
+                    rospy.loginfo("Executing place task")
+                    self.task.place(tinfo)
+            if key_press == ord(self.controls['open_gripper']):
+                rospy.loginfo("Opening gripper")
+                self.task.open_gripper()
+            if key_press == ord(self.controls['close_gripper']):
+                rospy.loginfo("Closing gripper")
+                self.task.close_gripper()
+            if key_press == ord(self.controls['home']):
+                rospy.loginfo("Executing home task")
+                self.task.home()
+            if key_press == ord(self.controls['stop_execution']):
+                rospy.loginfo("Stopping all execution")
+                self.task.stop()
+            if self.task.is_valid and key_press == ord(self.controls['add_step']):
+                if self.task.data_exists():
+                    self.task.save_task_to_chain()
+                    self.pick_data_list.append(self.pick_data)
+                    self.place_data_list.append(self.place_data)
+                    rospy.loginfo("task saved to chain")
+                else:
+                    rospy.logwarn("invalid task")
+
+    def save_demo(self) -> None:
+        """Save demonstration data"""
+        if self.lang_goal == "":
+            rospy.logwarn("Language goal must be set to save demonstration")
+            return
+        elif self.snapshot is None or len(self.snapshot) != 2:
+            rospy.logwarn("Valid snapshot pair (RGB-D) is not found")
+            return
+        elif self.task is None:
+            rospy.logwarn("Task executor is not set")
+            return
+        else:
+            self.task.save_task_to_chain()
+            self.pick_data_list.append(self.pick_data)
+            self.place_data_list.append(self.place_data)
+        if None in self.task.pick_pose[:-1] or None in self.task.place_pose[:-1]:
+            rospy.logwarn("Action pair (pick-place) pose not found or chain contains broken steps")
+            return
+        
+        action = {}
+        pick_list = self.task.pick_pose[:-1]
+        place_list = self.task.place_pose[:-1]
+        ind = 0
+        for pos in range(len(pick_list)):
+            pick_key = f"pose{ind}"
+            ind += 1
+            place_key = f"pose{ind}"
+            ind += 1
+
+            action[pick_key] = pick_list[pos]
+            action[place_key] = place_list[pos]
+
+        # action = {'pose0': self.task.pick_pose, 'pose1': self.task.place_pose}
+        color, depth = self.snapshot[0], self.snapshot[1]
+
+        # NOTE: currently supporting only a single lang goal per multitask
+        info = {'lang_goal': self.lang_goal, 'pick_data': self.pick_data_list, 'place_data': self.place_data_list}
+        # info = {'lang_goal': self.lang_goal, 'pick_data': self.pick_data, 'place_data': self.place_data}
+
+        rospy.loginfo("Saving demonstration...")
+        out_data = {'color': color, 'depth': depth, 'info': info, 'action': action}
+        filepath = Path(self.output['directory'], self.output['taskname'])
+        filename = f"{(len(list(Path(filepath).rglob('*'))) +1):04d}.pkl"
+        Path(filepath).mkdir(exist_ok=True, parents=True)
+        with open(Path(filepath, filename), "wb") as fd:
+            pickle.dump(out_data, fd)   
+
+        # Reset lang goal for next demonstration
+        self.lang_goal = ""
+
+        # Reset pick & place data for next demonstration
+        self.pick_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+        self.pick_data_list = []
+        self.place_data = {'rotation': 0, 'bbox': [], 'rotline': []}
+        self.place_data_list = []
+        self.task.reset_task()
+
+    def get_coords(self, action, x, y, flags, *userdata) -> None:
+        """Mouse callback function which is used to collect bbox coords"""
+        # Start marking the pick bbox/rotation
+        if action == cv2.EVENT_LBUTTONDOWN:
+            self.selected_tool = ToolType.PICKROTATE
+            if len(self.pick_data['bbox']) == 0:
+                self.selected_tool = ToolType.PICKBBOX
+                self.pick_data['bbox'] = [(x, y)]
+        # Release and freeze the pick bbox/rotation value
+        elif action == cv2.EVENT_LBUTTONUP:
+            if self.selected_tool is ToolType.PICKBBOX:
+                rospy.loginfo(f"Captured pick bbox coordinates: {self.pick_data['bbox']}")
+            if self.selected_tool is ToolType.PICKROTATE:
+                rospy.loginfo(f"Captured pick rotation angle: {self.pick_data['rotation']}")
+            self.selected_tool = ToolType.NONE
+        # Start marking the place bbox/rotation
+        elif action == cv2.EVENT_RBUTTONDOWN:
+            self.selected_tool = ToolType.PLACEROTATE
+            if len(self.place_data['bbox']) == 0:
+                self.selected_tool = ToolType.PLACEBBOX
+                self.place_data['bbox'] = [(x,y)]
+        # Release and freeze the place bbox/rotation value
+        elif action == cv2.EVENT_RBUTTONUP:
+            if self.selected_tool is ToolType.PLACEBBOX:
+                rospy.loginfo(f"Captured place bbox coordinates: {self.place_data['bbox']}")
+            if self.selected_tool is ToolType.PLACEROTATE:
+                rospy.loginfo(f"Captured place rotation angle: {self.place_data['rotation']}")
+            self.selected_tool = ToolType.NONE
+        # Assign the pick/place bbox/rotation value based on selected tool
+        elif action == cv2.EVENT_MOUSEMOVE:
+            # Handle bbox
+            if len(self.pick_data['bbox']) > 0 and self.selected_tool is ToolType.PICKBBOX:
+                self.pick_data['bbox'] = [self.pick_data['bbox'][0], (x, y)]
+            if len(self.place_data['bbox']) > 0 and self.selected_tool is ToolType.PLACEBBOX:
+                self.place_data['bbox'] = [self.place_data['bbox'][0], (x, y)]
+            # Handle rotation
+            data = None
+            if self.selected_tool is ToolType.PICKROTATE:
+                data = self.pick_data
+            if self.selected_tool is ToolType.PLACEROTATE:
+                data = self.place_data
+            if data:
+                line, theta = get_line_theta(data['bbox'], (x,y))
+                data['rotation'] = int(theta // self.rotation_angles)
+                data['rotline'] = line
+
+
+class ViewerGUI:
+    """Data viewer gui class"""
+
+    def __init__(self, config) -> None:
+        """Initialize class"""
+        self.viewer_win = "viewer"
+        self.stream_type = StreamType.RGB
+        files = list(Path(config['output']['directory'], config['output']['taskname']).rglob('*.pkl'))
+        files.sort()
+        if len(files) == 0:
+            raise KeyboardInterrupt("No data file found for viewing")
+        self.data = []
+        for file in files:
+            with open(file, "rb") as fd:
+                data = pickle.load(fd)
+                data['filename'] = file.stem
+                self.data.append(data)
+        self.controls = config["viewer_controlkeys"]
+        self.current_idx = 0
+        self.max_idx = len(self.data) - 1
+        cv2.namedWindow(self.viewer_win)
+
+    def cleanup(self):
+        """Release resources"""
+        cv2.destroyAllWindows()
+
+    def run(self) -> None:
+        """Run GUI"""
+        self.handle_viewer()
+        key_press = cv2.waitKey(1) & 0xFF
+        self.handle_keypress(key_press)
+
+    def handle_viewer(self) -> None:
+        """Handle viewing logic"""
+        snap_type = self.stream_type is not StreamType.RGB
+        data = self.data[self.current_idx]
+        filename = data['filename']
+        snapshot = [data['color'], data['depth']]
+        pick_data = data['info']['pick_data']
+        place_data = data['info']['place_data']
+        lang_goal = data['info']['lang_goal']
+
+        # info = {'lang_goal': self.lang_goal, 'pick_data': self.pick_data, 'place_data': self.place_data}
+        
+        # out_data = {'color': color, 'depth': depth, 'info': info, 'action': action}
+
+        disp_img = snapshot[snap_type]
+        # convert mono depth to color heatmap before displaying
+        if snapshot[snap_type].ndim == 2:
+            disp_img = depth_to_heatmap(disp_img)
+        else:
+            disp_img = cv2.cvtColor(disp_img, cv2.COLOR_RGB2BGR)
+        # Draw and display image
+        rot_color = (0, 0, np.iinfo(disp_img.dtype).max)
+        pick_bbox_color = (0, np.iinfo(disp_img.dtype).max, 0)
+        place_bbox_color = (np.iinfo(disp_img.dtype).max, 0, 0)
+        draw_on_disp_img(disp_img, pick_data, pick_bbox_color, rot_color)
+        draw_on_disp_img(disp_img, place_data, place_bbox_color, rot_color, f"{filename}: {lang_goal}")
+        cv2.imshow(self.viewer_win, disp_img)
+
+    def handle_keypress(self, key_press) -> None:
+        """Handle keypress logic"""
+        if key_press == ord(self.controls['rgb']):
+            self.stream_type = StreamType.RGB
+        if key_press == ord(self.controls['depth']):
+            self.stream_type = StreamType.DEPTH
+        if key_press == ord(self.controls['next']):
+            if self.current_idx < self.max_idx:
+                self.current_idx += 1
+        if key_press == ord(self.controls['previous']):
+            if self.current_idx > 0:
+                self.current_idx -= 1
+        if key_press == ord(self.controls['quit']):
+            raise KeyboardInterrupt
